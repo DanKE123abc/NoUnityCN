58e2bd94ae80db89cfcce49a9816abf016760007
DanKe <1586017466@QQ.com>
2025-03-02 17:59:10 +0800

    优化 SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { cn } from "@/lib/utils"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "NoUnityCN | Unity国际版下载站",
-  description: "NoUnityCN - Unity国际版下载站 | NoUnityCN是一项大家一起实现的开源项目，我们旨在为部分特殊地区的Unity开发者提供与世界上大多数用户一致的Unity Editor下载方式。支持通过直链和Unity Hub下载Unity Editor，支持下载Unity Build Support组件。",
-  keywords: "Unity国际版下载,Unity免中国版,Unity海外版,绕过Unity中国版,Unity汉化教程,团结引擎,Unity6,Unity6000,Unity下载站,Unity引擎下载站,Unity Hub,下载unity国际版,unityhub海外版,unity6000,unity镜像站,国际版unity下载,unity国际版,unity国际版下载,unity下载,unityhub国际版,unityhub,unityhub国际版下载,nounitycn,unity国际,国际版unity,unity6下载,unity海外版下载下载国际版,Unity组件下载,Unity Build Support,Unity Editor"
+  title: "NoUnityCN | Unity国际版下载站 - 为游戏开发者做一个安静的工具站",
+  description: "NoUnityCN - Unity国际版下载站 | NoUnityCN是一项开源项目，在为部分特殊地区的开发者提供国际版的Unity下载方式。支持通过直链和Unity Hub下载Unity，支持为Unity添加组件。",
+  keywords: "Unity6国际版,Unity组件下载,国际版Unity下载,Unity Hub国际版,UnityHub海外版,Unity引擎下载站,Unity6下载,Unity6000,Unity海外版下载,Unity国际版下载"
 }
 
 export default function RootLayout({
